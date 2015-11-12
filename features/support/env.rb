ef26d3fae80bd412c5be7414f8c81ecf06954d52
Ashwin Gonsalves <ashwingonsalves@gmail.com>
2015-11-12 11:47:27 +0530

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,23 +1,26 @@
 # before all
 environment = :remote # :local or :remote
-username = "" # get it from https://saucelabs.com/account
-api_key = "" # get it from https://saucelabs.com/account
+username = "<username>"
+api_key = "<access_key>"
 
 require "bundler/setup"
 require "watir-webdriver"
 
 def browser_for(environment, username, api_key)
   if environment == :remote
-    caps = Selenium::WebDriver::Remote::Capabilities.firefox
-    caps.version = "11"
-    caps.platform = :VISTA
+    caps = Selenium::WebDriver::Remote::Capabilities.new
+    caps[:os] = "Windows"
+    caps[:browser] = "firefox"
+    caps[:browser_version] = "41.0"
+    caps["browserstack.debug"] = "true"
     caps[:name] = "#{environment} #{ENV['TEST_ENV_NUMBER']}"
 
     require "selenium/webdriver/remote/http/persistent" # :http_client
+    
     Watir::Browser.new(
       :remote,
       :http_client => Selenium::WebDriver::Remote::Http::Persistent.new,
-      :url => "http://#{username}:#{api_key}@ondemand.saucelabs.com:80/wd/hub",
+      :url => "http://#{username}:#{api_key}@hub.browserstack.com/wd/hub",
       :desired_capabilities => caps)
   else
     Watir::Browser.new :firefox
