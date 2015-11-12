d277ec60ae36cf1b8c1ddbda30b010c96ee5a0d0
Ashwin Gonsalves <ashwingonsalves@gmail.com>
2015-11-12 18:00:32 +0530

    Update google_steps.rb

--- features/step_definitions/google_steps.rb
+++ features/step_definitions/google_steps.rb
@@ -4,9 +4,9 @@ end
 
 When /^I search Google for (.*)$/ do |term|
   @browser.text_field(name: "q").set term
-  @browser.button(id: "gbqfb").click
+  #@browser.button(id: "gbqfb").click
 end
 
 Then /^I should see (.*) at Google search results$/ do |term|
-  @browser.em(text: term).when_present.should exist
+  @browser.link(text: term).when_present.should exist
 end
