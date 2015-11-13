10414b99c93268e3f1e0931b24c663086e391b55
Ashwin Gonsalves <ashwingonsalves@gmail.com>
2015-11-13 16:24:31 +0530

    Update yahoo_steps.rb

--- features/step_definitions/yahoo_steps.rb
+++ features/step_definitions/yahoo_steps.rb
@@ -1,9 +1,11 @@
 When /^I search Yahoo for (.*)$/ do |term|
   @browser.text_field(name: "p").set term
-  @browser.button(id: "UHSearchWeb").click
+  #@browser.driver.find_element(:id, "UHSearchWeb").click
+  @browser.button(id: "search-submit").click
+
 end
 
 Then /^I should see (.*) at Yahoo search results$/ do |term|
-  #@browser.link(text: term).should exist
-  @browser.link(text: term).when_present.should exist
+  @browser.link(text: term).should exist
+  #@browser.link(text: term).when_present.should exist
 end
